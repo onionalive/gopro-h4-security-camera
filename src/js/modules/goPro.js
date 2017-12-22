76b33f8ca6b4887a35d4beffa0b28e932788cee1
Stephen <stephenritchie0@gmail.com>
2017-12-22 12:48:04 -0800

    Update camera get status logic and display

--- src/js/modules/goPro.js
+++ src/js/modules/goPro.js
@@ -4,13 +4,29 @@ import GoPro from 'goproh4';
  */
 class goPro {
 	constructor() {
-		this.camera = new GoPro.Camera();
+		this.camera = new GoPro.Camera({
+			mac: [212, 217, 25, 203, 194, 173]
+		});
 		this.media = [];
 	}
 
 	init() {
 		this.camera.ready();
+		this.getCameraData();
 		this.clickEventhandlers();
+	}
+
+	getCameraData() {
+		let macAddressArray = this.camera._mac;
+		let macAddress = '';
+		for (let i = 0; i < this.camera._mac.length; i++) {
+			macAddress += this.camera._mac[i];
+		}
+
+		$('.broadcastip span').text(this.camera._broadcastip);
+		$('.ip span').text(this.camera._ip);
+		$('.mac span').text(macAddress);
+		$('.ready span').text(this.camera._ready);
 	}
 
 	clickEventhandlers() {
@@ -23,6 +39,7 @@ class goPro {
 		});
 
 		$('.show-status').on('click', () => {
+			this.getCameraData();
 			console.log(this.camera);
 		});
 
